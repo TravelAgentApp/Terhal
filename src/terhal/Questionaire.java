445cf86020dc8d1e4331eb3e1c51dc8e21b1e583
Jzakai <Jana.zakai@gmail.com>
2024-09-24 11:39:09 +0300

    first comit

---
+++ src/terhal/Questionaire.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package terhal;
+
+/**
+ *
+ * @author janaz
+ */
+public class Questionaire {
+ String questionId; 
+ int members; 
+int members;
+int TravelPurpose;
+TravelPlan travelPlan; 
+         - flightId: FlightId
+ - hotelId: HotelId
+         - activityId: ActivityId 
+                 - restaurantId: RestaurantId
+ - Weather: String
+         -TravelDuration: date
+ - budget: float
+         - user: User
+                 - questions: List<String>
+ - countries: Countries
+         - responses: List<String>
+}
