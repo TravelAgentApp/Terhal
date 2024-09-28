f4bd342b949bf80096cd005e16e686e0d625f422
Jzakai <Jana.zakai@gmail.com>
2024-09-28 15:50:54 +0300

    added setter and getter for user and questionare classes

--- src/terhal/Questionaire.java
+++ src/terhal/Questionaire.java
@@ -24,11 +24,121 @@ public class Questionaire {
     int ActivityId;
     int RestaurantId;
     String Weather;
-    //date TravelDuration;
     String TravelDuration;
     float budget;
     User user ;
-    List<String> questions;
+    //List<String> questions;
     Countries countries ;
-    List<String> responses;
+    //List<String> responses;
+    
+    public Questionaire () {
+        
+    }
+
+    public String getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(String questionId) {
+        this.questionId = questionId;
+    }
+
+    public int getMembers() {
+        return members;
+    }
+
+    public void setMembers(int members) {
+        this.members = members;
+    }
+
+    public int getTravelPurpose() {
+        return TravelPurpose;
+    }
+
+    public void setTravelPurpose(int TravelPurpose) {
+        this.TravelPurpose = TravelPurpose;
+    }
+
+    public TravelPlan getTravelPlan() {
+        return travelPlan;
+    }
+
+    public void setTravelPlan(TravelPlan travelPlan) {
+        this.travelPlan = travelPlan;
+    }
+
+    public int getFlightId() {
+        return FlightId;
+    }
+
+    public void setFlightId(int FlightId) {
+        this.FlightId = FlightId;
+    }
+
+    public int getHotelId() {
+        return HotelId;
+    }
+
+    public void setHotelId(int HotelId) {
+        this.HotelId = HotelId;
+    }
+
+    public int getActivityId() {
+        return ActivityId;
+    }
+
+    public void setActivityId(int ActivityId) {
+        this.ActivityId = ActivityId;
+    }
+
+    public int getRestaurantId() {
+        return RestaurantId;
+    }
+
+    public void setRestaurantId(int RestaurantId) {
+        this.RestaurantId = RestaurantId;
+    }
+
+    public String getWeather() {
+        return Weather;
+    }
+
+    public void setWeather(String Weather) {
+        this.Weather = Weather;
+    }
+
+    public String getTravelDuration() {
+        return TravelDuration;
+    }
+
+    public void setTravelDuration(String TravelDuration) {
+        this.TravelDuration = TravelDuration;
+    }
+
+    public float getBudget() {
+        return budget;
+    }
+
+    public void setBudget(float budget) {
+        this.budget = budget;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Countries getCountries() {
+        return countries;
+    }
+
+    public void setCountries(Countries countries) {
+        this.countries = countries;
+    }
+
+  
+    
 }
--- src/terhal/User.java
+++ src/terhal/User.java
@@ -1,10 +1,82 @@
 
 package terhal;
 
+import java.util.UUID;
+
 /**
  *
  * @author s4ooo
  */
-class User {
+public class User {
+    private String userId;
+    private String name;
+    private String email;
+    private String preferences;
+    private String travelPlan;
+    private float budget;
+    private Questionaire UserQuestionaire;
+    //private List<TravelPlan> travelHistory;
+
+    public User() {
+        this.userId = UUID.randomUUID().toString();
+    }
+
+    
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPreferences() {
+        return preferences;
+    }
+
+    public void setPreferences(String preferences) {
+        this.preferences = preferences;
+    }
+
+    public String getTravelPlan() {
+        return travelPlan;
+    }
+
+    public void setTravelPlan(String travelPlan) {
+        this.travelPlan = travelPlan;
+    }
+
+    public float getBudget() {
+        return budget;
+    }
+
+    public void setBudget(float budget) {
+        this.budget = budget;
+    }
+
+    public Questionaire getUserQuestionaire() {
+        return UserQuestionaire;
+    }
+
+    public void setUserQuestionaire(Questionaire UserQuestionaire) {
+        this.UserQuestionaire = UserQuestionaire;
+    }
+    
     
 }
